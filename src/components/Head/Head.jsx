57b1f40eea25f562aed477120ba36f6312a2898f
fuckthehomework-tw <antony19931013@gmail.com>
2025-03-27 21:26:56 +0800

    Add files via upload

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Head.css'
+
+const Head = () => {
+  return (
+    <>
+    <header className="header">
+    <h1 className="title"> Book store</h1>
+  </header>
+    </>
+  )
+}
+
+export default Head
